56711530788010b5c08c9d64643b90d1a1d2ec38
Adrian Skorski Garcia <adrian.skorski.garcia@gmail.com>
2026-06-09 13:27:11 +0200

    Initial commit

---
+++ src/main/java/com/adrianmaria/app_habitos/AppHabitosApplication.java
@@ -0,0 +1,13 @@
+package com.adrianmaria.app_habitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHabitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHabitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrianmaria/app_habitos/AppHabitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianmaria.app_habitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHabitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
